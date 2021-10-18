c269ba6e82474af0ccab844af3c73d6a576d1d7f
Michael Nogueira <michaelnogueira.8237@aluno.saojudas.br>
2021-10-17 22:45:29 -0300

    inclusão de novo Angular  login e signup estão separados nesse

--- AppTalkie/karma.conf.js
+++ AppTalkie/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/appMensagem'),
+      dir: require('path').join(__dirname, './coverage/AppTalkie'),
       subdir: '.',
       reporters: [
         { type: 'html' },
